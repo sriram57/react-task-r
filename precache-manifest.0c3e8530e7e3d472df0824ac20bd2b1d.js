75b964e19e9ebe05a0be4322deb948dae2787bc7
Sriram <vinaysriramvsr007@gmail.com>
2020-07-14 15:14:25 +0530

    Updates

---
+++ precache-manifest.0c3e8530e7e3d472df0824ac20bd2b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d552affea0645bd0b04895292c06d25",
+    "url": "/react-task-resume/index.html"
+  },
+  {
+    "revision": "8f7094e656b97db15dd1",
+    "url": "/react-task-resume/static/css/main.99398287.chunk.css"
+  },
+  {
+    "revision": "9a6d846208810f59e096",
+    "url": "/react-task-resume/static/js/2.d6caecaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-task-resume/static/js/2.d6caecaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7094e656b97db15dd1",
+    "url": "/react-task-resume/static/js/main.c60a9b7b.chunk.js"
+  },
+  {
+    "revision": "5e86c6f16a5ea7d66668",
+    "url": "/react-task-resume/static/js/runtime-main.99e9148e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-task-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
